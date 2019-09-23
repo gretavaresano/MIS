8bb6d4774ecead791e1cf9e611f0564544f85650
gretavaresano <greta.varesano@gmail.com>
2019-09-23 11:45:01 +0200

    prova5

--- assets/scripts/mouse.js
+++ assets/scripts/mouse.js
@@ -1,6 +1,14 @@
 if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
   $(document).mousemove(function(event){
     $('.mouse-container').append('<div class="mouse-print" style="top:' + event.pageY + 'px; left:' + event.pageX + 'px;"></div>');
-
   });
 }
+
+
+window.addEventListener("touchmove", function(event) {
+	var touch;
+	if (event.targetTouches.length == 1) {
+		touch = event.targetTouches[0];
+  $('.mouse-container').append('<div class="mouse-print" style="top:' + event.pageY + 'px; left:' + event.pageX + 'px;"></div>');
+	}
+});
