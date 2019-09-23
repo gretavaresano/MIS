434d2bffc2a55797db99ea0d56242e8c3e13a8d2
gretavaresano <greta.varesano@gmail.com>
2019-09-23 11:24:52 +0200

    cambiamenti

--- italiano/assets/scripts/main.js
+++ italiano/assets/scripts/main.js
@@ -17,30 +17,8 @@ function checkTime(i) {
 
 
 
-window.addEventListener("load", startTime2, true);
-
-function startTime2() {
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-
-  m = checkTime(m);
-
-  document.getElementById('clock2').innerHTML =
-  h + ":" + m;
-  var t = setTimeout(startTime2, 500);
-}
-function checkTime(i) {
-  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-  return i;
-}
-
 $(window).on('afterunload', function(){
   $(window).scrollTop(0);
   document.body.scrollTop = 0;
  document.documentElement.scrollTop = 0; //
 });
-
-
-
-startTime2();
