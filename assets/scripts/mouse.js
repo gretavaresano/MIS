f8ac900a8bca4b531567a4395e60749042b749d8
gretavaresano <greta.varesano@gmail.com>
2019-09-23 14:56:34 +0200

    //

--- assets/scripts/mouse.js
+++ assets/scripts/mouse.js
@@ -1,5 +1,3 @@
-if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
   $(document).mousemove(function(event){
     $('.mouse-container').append('<div class="mouse-print" style="top:' + event.pageY + 'px; left:' + event.pageX + 'px;"></div>');
   });
-}
