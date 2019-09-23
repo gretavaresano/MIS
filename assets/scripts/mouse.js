e501df103c6209b3e8354fbbde8786d8c7c2e6bc
gretavaresano <greta.varesano@gmail.com>
2019-09-23 11:48:09 +0200

    cambiamenti ita en

--- assets/scripts/mouse.js
+++ assets/scripts/mouse.js
@@ -3,12 +3,3 @@ if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Pho
     $('.mouse-container').append('<div class="mouse-print" style="top:' + event.pageY + 'px; left:' + event.pageX + 'px;"></div>');
   });
 }
-
-
-window.addEventListener("touchmove", function(event) {
-	var touch;
-	if (event.targetTouches.length == 1) {
-		touch = event.targetTouches[0];
-  $('.mouse-container').append('<div class="mouse-print" style="top:' + event.pageY + 'px; left:' + event.pageX + 'px;"></div>');
-	}
-});
